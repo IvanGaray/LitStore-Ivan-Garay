5c1c0874213787ed859a0bf6045fb8087b44b302
IvanGaray <68177252+IvanGaray@users.noreply.github.com>
2021-10-04 14:18:11 -0300

    Se agrega el logo al nav.
    Se agrega un logo con otro diseño y un png de carrito en la navbar

--- src/components/CartWidget/CartWidget.js
+++ src/components/CartWidget/CartWidget.js
@@ -1,9 +1,11 @@
+import "./CartWidget.css";
 import React from 'react'
+import img from '../../assets/carrito.png'
 
 const CartWidget = () => {
     return (
         <div>
-            
+            <img src={img} width='70px' height='50px'/>
         </div>
     )
 }
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,33 +1,35 @@
 import "./NavBar.css";
 import React from 'react'
 import logo from '../assets/logo.png'
+import CartWidget from '../components/CartWidget/CartWidget'
 
 const NavBar = () => {
     return (
+
         <div>
-            <img src={logo} width='70px' height='50px'/>
-           <div className="brand">LitStore</div>
             
-        <nav className="NavBar">
-        
-
-            <div class="">
             
-                <ul class="">
-                    <li class="">
-                        <a class="" href="#">Inicio</a>
-                    </li>
-                    <li class="">
-                        <a class="" href="#">Productos</a>
-                    </li>
-                    <li class="">
-                        <a class="" href="#">Servicios</a>
-                    </li>
-                </ul>
+            <nav className="NavBar">
+            <CartWidget/>
+            <img className='logo' src={logo} width='70px' height='50px'/>
 
-            </div>
+                <div className="">
+                
+                    <ul className="">
+                        <li className="">
+                            <a className="" href="#">Inicio</a>
+                        </li>
+                        <li className="">
+                            <a className="" href="#">Productos</a>
+                        </li>
+                        <li className="">
+                            <a className="" href="#">Servicios</a>
+                        </li>
+                    </ul>
 
-        </nav>
+                </div>
+
+            </nav>
         
         </div>
     )
